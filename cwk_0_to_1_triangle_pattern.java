2cd08a54f6ecdca8524e538e7ce1133d1cfad593
im-kamil <kamilnasar786@gmail.com>
2021-12-29 00:00:01 +0530

    floydtriangle

---
+++ cwk_0_to_1_triangle_pattern.java
@@ -0,0 +1,20 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class cwk_0_to_1_triangle_pattern {
+    public static void main(String[] args) {
+        int n = 5;
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                int sum = i + j;
+                if (sum % 2 == 0) { // even
+                    System.out.print("1 ");
+                } else { // odd
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
--- cwk_floydtriangle.java
+++ cwk_floydtriangle.java
@@ -4,7 +4,7 @@ public class cwk_floydtriangle {
         int number = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(number);
+                System.out.print(number+ " ");
                 number++; // increase by 1 means number++ as per question number=number+1;
             }
             System.out.println();
