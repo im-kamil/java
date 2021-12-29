cb3ce303630e66530da88b07908d3291530773ea
im-kamil <kamilnasar786@gmail.com>
2021-12-30 04:16:44 +0530

    package.java

--- cwk_diamondpattern.java
+++ cwk_diamondpattern.java
@@ -4,7 +4,7 @@ public class cwk_diamondpattern {
         // upper half loop
         for (int i = 1; i <= n; i++) {
             // for spaces
-            for (int j = 1; j <= n - 1; j++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
             // stars
@@ -16,7 +16,7 @@ public class cwk_diamondpattern {
         // lower half loop
         for (int i = n; i >= 1; i--) {
             // for spaces
-            for (int j = 1; j <= n - 1; j++) {
+            for (int j = i; j <= n - 1; j++) {
                 System.out.print(" ");
             }
             // stars
