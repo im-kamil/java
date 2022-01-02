84955179c064d816f7194d6bb3569e4ad80c712a
im-kamil <kamilnasar786@gmail.com>
2022-01-03 02:21:47 +0530

    ERRORS.JAVA

---
+++ cwk_79_ch14_errors.java
@@ -0,0 +1,7 @@
+public class cwk_79_ch14_errors {
+   public static void main(String[] args) {
+       int a=350 //SYNTAX ERROR
+     //  int b=5;
+       System.out.println(a+b); //SYNTAX ERROR , VARIABLE IS NOT DECLARED
+   } 
+}
