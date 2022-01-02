e1bad7505396c56fdd589577eccf6074db173994
im-kamil <kamilnasar786@gmail.com>
2022-01-03 03:05:59 +0530

    try catch

--- cwk_81_try_catch.java
+++ cwk_81_try_catch.java
@@ -4,16 +4,16 @@ public class cwk_81_try_catch {
         int b = 0; // exception arthmtic because we should not divide by 0 but it can be resolve by
                    // try and catch method lets c
       // without try   
-      int c = a / b;
-      System.out.println("the result  is " + c);          
+    //   int c = a / b;
+    //   System.out.println("the result  is " + c);          
      // with try
-        // try {
-        //     int c = a / b;
-        //     System.out.println("the result  is " + c);
-        // } catch (Exception e) {
-        //     System.out.println("we failed to divide reason...");
-        //     System.out.println(e);
-        // }
+        try {
+            int c = a / b;
+            System.out.println("the result  is " + c);
+        } catch (Exception e) {
+            System.out.println("we failed to divide reason...");
+            System.out.println(e);
+        }
         System.out.println("end of the programme");
 
     }
